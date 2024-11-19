6db042f691979d53b49699eeb76e6e17c713635b
Etornam <navidben45@gmail.com>
2024-11-19 22:09:56 +0000

    Bug fix: 🐞 Legend state node reolved

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,15 +1,12 @@
 import 'react-native-get-random-values';
 import { createClient } from '@supabase/supabase-js'
-import { configureSyncedSupabase } from '@legendapp/state/sync-plugins/supabase'
 import { v4 as uuidv4 } from 'uuid';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Create a configured sync function
 // Example generateId function
 export const generateId = () => uuidv4();
-configureSyncedSupabase({
-    generateId,
-})
+
 
 export const supabase = createClient(
     process.env.EXPO_PUBLIC_SUPABASE_URL as string,
