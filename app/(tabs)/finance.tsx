0f51493cf70b359de6cc248d9a4dabd687c75d65
Etornam <navidben45@gmail.com>
2024-11-19 22:02:10 +0000

    Add testing code here

--- app/(tabs)/finance.tsx
+++ app/(tabs)/finance.tsx
@@ -1,11 +1,33 @@
-import { Text, View } from 'tamagui'
+import database, { inventoryCollection, productsCollection } from 'model'
+import { Button, Text, View } from 'tamagui'
 
 export default function FinancePage() {
+  function Test() {
+    database.write(async () => {
+      const _products = await productsCollection.query().fetch()
+      console.log(_products)
+      // create a new inventory
+      const _inventory = await inventoryCollection.create((inventory) => {
+        inventory.price = 100
+        inventory.stock = 100
+        inventory.business.set(null)
+        inventory.productId = _products[0].id
+      }).then(() => {
+        console.log('Inventory created')
+      }).catch((error) => {
+        console.log(error)
+      })
+      console.log(_inventory)
+    })
+  }
+
   return (
     <View>
-      <Text fontSize={20} color="$blue10">
-        Finance
-      </Text>
+      <Button onPress={Test} >
+        <Text fontSize={20} color="$blue10">
+          Finance
+        </Text>
+      </Button>
     </View>
   )
 }
