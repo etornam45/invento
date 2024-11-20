40ef8cd3ae92b1c31df359a996ecd48f9275c57e
Etornam <navidben45@gmail.com>
2024-11-20 02:16:02 +0000

    created all associations to sale

--- model/db/sales.ts
+++ model/db/sales.ts
@@ -4,9 +4,15 @@ import { children, date, field, readonly, relation, text,  } from '@nozbe/waterm
 export default class Sale extends Model {
     static table: string = 'sale'
 
+    static associations = {
+        sale_items: { type: 'has_many' as const, foreignKey: 'sale_id' },
+        business: { type: 'belongs_to' as const, key: 'business_id' },
+        users: { type: 'belongs_to' as const, key: 'user_id' },
+    }
+
     @field('total') total!: number;
-    @field('payment') payment!: number;
-    @field('amount') amount!: number;
+    @field('payment') payment!: string;
+    @field('quantity') quantity!: number;
 
     @field('business_id') businessId!: string;
     @field('user_id') userId!: string;
