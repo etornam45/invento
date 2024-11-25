27d536a4acddcb94eab743c2140bfcda4e3c5398
Etornam <navidben45@gmail.com>
2024-11-25 21:29:32 +0000

    Added Icons on index page

---
+++ icons/SolarMagniferLinear.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Svg, { Path, G, Circle, Line } from 'react-native-svg';
+
+export function SolarMagniferLinear(props) {
+ return (
+    <Svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><G  strokeWidth="1.5"><Circle cx="11.5" cy="11.5" r="9.5"></Circle><Path strokeLinecap="round" d="M18.5 18.5L22 22"></Path></G></Svg>
+ )
+}
+export default SolarMagniferLinear
