818874ac9ea9d39d9faeff212a871c65474d61a9
Etornam <navidben45@gmail.com>
2024-11-25 21:32:51 +0000

    create utility to calulate the time passed over a timestamp

--- components/cards/SalesCard.tsx
+++ components/cards/SalesCard.tsx
@@ -5,10 +5,11 @@ import { BadgeCent, Barcode } from "@tamagui/lucide-icons";
 import sales from "app/(tabs)/sales";
 import { router } from "expo-router";
 import { timeAgo } from "lib/utils";
-import { salesItemCollection } from "model";
+import database, { salesCollection, salesItemCollection } from "model";
 import Products from "model/db/products";
 import Sale from "model/db/sales";
 import SalesItem from "model/db/sales_item";
+import { useEffect, useState } from "react";
 import { from, map, toArray } from "rxjs";
 import { Text, View, XStack, YStack } from "tamagui";
 
@@ -17,30 +18,46 @@ interface SalesCardProp {
   salesItemCount: SalesItem[]
 }
 
-const SalesCard = ({ sale, salesItemCount }: SalesCardProp) => {
-  return (<View mt='$3.5' p='$3.5' backgroundColor='$background' borderRadius={12} onPress={() => router.push(`/sales/${sale.id}`)}>
+const SalesCard = ({ sale_id }: {sale_id: string}) => {
+
+  const [sale, setSale] = useState<Sale | null>(null);
+  const [salesItemCount, setSalesItemCount] = useState<number>();
+
+  useEffect(() => {
+    async function fetchData() {
+      await database.write(async () => {
+        const __sale = await salesCollection.find(sale_id)
+        const __salesItemCount = await salesItemCollection.query(Q.where('sale_id', __sale.id)).fetch()
+        setSale(__sale)
+        setSalesItemCount(__salesItemCount.length)
+      })
+    }
+    fetchData()
+  }, [])
+
+  return (<View p='$3.5' backgroundColor='$background' onPress={() => router.push(`/sales/${sale_id}`)}>
     <YStack gap='$1.5'>
       <XStack w='100%' jc='space-between' ai='flex-start'>
         <YStack  >
           <Text fontSize={15} fontWeight='bold'>
-            {salesItemCount.length} items
+            {salesItemCount} items
           </Text>
         </YStack>
         {<Text fontSize={12}>
-          {timeAgo(sale.createdAt)}
+          {sale?.createdAt ? timeAgo(sale?.createdAt) : 'No date'}
         </Text>}
       </XStack>
       <XStack>
         <View w='50%'>
           <Text>
             <Text fontWeight='900'>GHC </Text>
-            {sale.total.toFixed(2)}
+            {sale?.total.toFixed(2)}
           </Text>
         </View>
         <View w='50%'>
           <Text>
             <Text fontWeight='900'>QTY </Text>
-            {sale.quantity}
+            {sale?.quantity}
           </Text>
         </View>
       </XStack>
@@ -51,16 +68,13 @@ const SalesCard = ({ sale, salesItemCount }: SalesCardProp) => {
         </XStack>
         <XStack w='50%' gap='$1' ai='center'>
           <BadgeCent size={20} color='$blue12Light' />
-          <Text>{sale.payment}</Text>
+          <Text>{sale?.payment}</Text>
         </XStack>
       </XStack>
     </YStack>
   </View>)
 }
 
-const enhance = withObservables(['sale'], ({ sale }: { sale: Sale }) => ({
-  sale,
-  salesItemCount: salesItemCollection.query(Q.where('sale_id', sale.id)).observe()
-}))
 
-export default enhance(SalesCard);
+
+export default SalesCard;
